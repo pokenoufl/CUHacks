80c89f0491ce813e28a37f4e2d107628f6980f9c
pokenoufl <liam.dehaudt@gmail.com>
2016-06-12 06:37:55 -0700

    finishing final version

--- app/src/main/java/cuhacks/cuhacks1/InfoActivity.java
+++ app/src/main/java/cuhacks/cuhacks1/InfoActivity.java
@@ -2,6 +2,7 @@ package cuhacks.cuhacks1;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Window;
 
 public class InfoActivity extends AppCompatActivity {
 
--- app/src/main/java/cuhacks/cuhacks1/MapsActivity.java
+++ app/src/main/java/cuhacks/cuhacks1/MapsActivity.java
@@ -1,16 +1,16 @@
 package cuhacks.cuhacks1;
 
-import android.location.LocationManager;
-import android.support.v4.app.FragmentActivity;
-import android.os.Bundle;
+        import android.location.LocationManager;
+        import android.support.v4.app.FragmentActivity;
+        import android.os.Bundle;
 
-import com.google.android.gms.maps.CameraUpdateFactory;
-import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.OnMapReadyCallback;
-import com.google.android.gms.maps.SupportMapFragment;
-import com.google.android.gms.maps.UiSettings;
-import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.MarkerOptions;
+        import com.google.android.gms.maps.CameraUpdateFactory;
+        import com.google.android.gms.maps.GoogleMap;
+        import com.google.android.gms.maps.OnMapReadyCallback;
+        import com.google.android.gms.maps.SupportMapFragment;
+        import com.google.android.gms.maps.UiSettings;
+        import com.google.android.gms.maps.model.LatLng;
+        import com.google.android.gms.maps.model.MarkerOptions;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
@@ -48,9 +48,17 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
         mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
 
+        mMap.addMarker(new MarkerOptions().position(new LatLng(37.363007, -121.904068)).title("Next Door Solutions To Domestic Violence").snippet("234 E Gish Rd #200, San Jose, CA 95112"));
+        mMap.addMarker(new MarkerOptions().position(new LatLng(37.337731, -121.894176 )).title("Domestic Violence Intervention").snippet("111 N Market St, San Jose, CA 95113"));
+        mMap.addMarker(new MarkerOptions().position(new LatLng(37.374239, -122.029505)).title("YWCA Silicon Valley").snippet("375 S 3rd St, San Jose, CA 95112"));
+        mMap.addMarker(new MarkerOptions().position(new LatLng(37.372462, -122.038209)).title("A Turning Point-Sunnyvale").snippet("505 W Olive Ave, Sunnyvale, CA 94086"));
+        mMap.addMarker(new MarkerOptions().position(new LatLng(37.558439, -121.979534)).title("Safe Alternatives To Violent Environments").snippet("1900 Mowry Ave #201, Fremont, CA 94538"));
+        mMap.addMarker(new MarkerOptions().position(new LatLng(37.546624, -122.306173)).title("Cora").snippet("1633 Old Bayshore Hwy # 280, Burlingame, CA 94010"));
+
+
         UiSettings mapSettings;
         mapSettings = mMap.getUiSettings();
-        float maxZoom = 14.0f;
+        float maxZoom = 10.0f;
         mMap.animateCamera(CameraUpdateFactory.zoomTo(maxZoom));
 
         mapSettings.setZoomControlsEnabled(true);
